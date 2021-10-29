fbd4f89a60dab2b9947ec304533a5702202c5c56
Pierre <pierre.lagadec@gmail.com>
2021-10-29 11:49:09 +1100

    added middleware, types and example routes

--- dist/app.js
+++ dist/app.js
@@ -4,5 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const exampleRoutes_1 = __importDefault(require("./routes/exampleRoutes"));
 const app = (0, express_1.default)();
+app.use('/example', exampleRoutes_1.default);
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message });
+});
 app.listen(3000);
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import exampleRoutes from './routes/exampleRoutes';
 
 const app = express();
+
+app.use('/example', exampleRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json({message: err.message});
+});
 
 app.listen(3000);
